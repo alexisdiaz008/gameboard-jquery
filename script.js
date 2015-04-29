22d88d393298f8343b2f71bf2d3e3414289c5e06
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-04-29 17:19:08 -0400

    game

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var main = function() {
 
+$('.more-btn').click(function(){
+	// console.log(this);
+	// $('.more-menu').toggle();
+// console.log("clicking works")
+ $(this).next().toggle();
+	});
+	$('.share').click(function(){
+		$(this).next().toggle();
+	});
+	$('.notification').click(function(){
+		$(this).next().toggleClass('active');
+	});
+
 };
 
 $(document).ready(main);
